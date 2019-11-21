7b96b6a6582826559882528d31b656e2bf1780ba
Caio <caio_uechi@hotmail.com>
2019-11-20 20:31:24 -0500

    Comparing 2 products

--- EasyGroceriesApp/src/app/page1/page1.component.ts
+++ EasyGroceriesApp/src/app/page1/page1.component.ts
@@ -12,37 +12,61 @@ export class Page1Component implements OnInit {
   txtPriceB = new FormControl('');
   txtQuantityA = new FormControl('');
   txtQuantityB = new FormControl('');
+  txtMultiplyA = new FormControl('');
+  txtMultiplyB = new FormControl('');
 
   clickMessage = '';
   logResult = '';
   PriceA: number = 0;
   QuantityA: number = 0;
+  MultiplyA: number = 0;
+
   PriceB: number = 0;
   QuantityB: number = 0;
+  MultiplyB: number = 0;
+
   valorRandomico: number = 3;
 
   Compare() {
     var priceA = parseFloat(this.txtPriceA.value);
     var quantityA = parseFloat(this.txtQuantityA.value);
+    var MultiplyA = parseFloat(this.txtMultiplyA.value);
     var totalA = (priceA / quantityA);
+    if (MultiplyA > 0) {
+      totalA = totalA / MultiplyA
+    }
 
     var priceB = parseFloat(this.txtPriceB.value);
     var quantityB = parseFloat(this.txtQuantityB.value);
-    var totalB = (priceB * quantityB);
+    var MultiplyB = parseFloat(this.txtMultiplyB.value);
+    var totalB = (priceB / quantityB);
+    if (MultiplyB > 0) {
+      totalB = totalB / MultiplyB
+    }
 
     if (totalA > totalB) {
       var difference = totalA - totalB;
-      this.clickMessage = "Price B is cheaper. You will save " + difference.toFixed(2) + " per unit";
+      this.clickMessage = "Price B is cheaper. You will save " + difference.toFixed(2) + " per volume";
     } else if (totalA < totalB) {
       var difference = totalB - totalA;
-      this.clickMessage = "Price A is cheaper. You will save " + difference.toFixed(2) + " per unit";
+      this.clickMessage = "Price A is cheaper. You will save " + difference.toFixed(2) + " per volume";
     } else {
       this.clickMessage = "They are the same";
     }
   }
 
   Clear() {
-    this.cleanMessage();
+    this.cleanMessage()
+    this.cleanInputs()
+  }
+
+  cleanInputs() {
+    this.txtPriceA = new FormControl('');
+    this.txtPriceB = new FormControl('');
+    this.txtQuantityA = new FormControl('');
+    this.txtQuantityB = new FormControl('');
+    this.txtMultiplyA = new FormControl('');
+    this.txtMultiplyB = new FormControl('');
   }
 
   cleanMessage() {
@@ -51,8 +75,6 @@ export class Page1Component implements OnInit {
   }
 
   constructor() { }
-
   ngOnInit() {
   }
-
 }
